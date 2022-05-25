a6c6b51d53ca331808f4029ae432e1ce47ce2d0e
Amjad Khan <dajma00@hotmail.com>
2022-05-25 23:52:04 +0500

    QuickStart1

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -4,17 +4,20 @@ namespace IdentityServer;
 
 public static class Config
 {
+
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
+        {
             new IdentityResources.OpenId()
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
-    new List<ApiScope>
-    {
-        new ApiScope(name: "api1", displayName: "MyAPI")
-    };
+        new ApiScope[]
+            {
+                new ApiScope(name: "api1", displayName: "MyAPI")
+
+
+            };
 
     public static IEnumerable<Client> Clients =>
     new List<Client>
